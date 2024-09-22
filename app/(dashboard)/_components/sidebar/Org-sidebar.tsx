bd9092e3fee25dbf5c7e6731e3187172795d0dc2
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-09-22 09:05:42 +0530

    some changes in organizatoinal sidebar

--- app/(dashboard)/_components/sidebar/Org-sidebar.tsx
+++ app/(dashboard)/_components/sidebar/Org-sidebar.tsx
@@ -8,6 +8,7 @@ import { OrganizationSwitcher } from "@clerk/nextjs"
 import { Button } from "@/components/ui/button"
 import {  LayoutDashboard, Star } from "lucide-react"
 import { useSearchParams } from "next/navigation"
+import Image from "next/image"
 
 const font = Poppins ({
   subsets: ['latin'],
@@ -21,16 +22,16 @@ export const OrgSidebar =  () =>  {
 
 
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5  text-white rounded-md ml-3 ">
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5  text-white rounded-md ml-3  ">
       <Link href="/">
         <div className="flex items-center gap-x-2">
-          {/* logo implementation here 
+      
            <Image 
-           src="/logo.svg"
+           src="/icons/logo2.svg"
            alt="Logo"
-           height={20}
-           width={20}
-           />  */}
+           height={50}
+           width={50}
+           /> 
 
       <span className= {cn("font-semibold text-2xl",
         font.className,)}>
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,36 +1,4 @@
-// // Implementation of dashboard image
-// "use client";
 
-// import { useOrganization } from "@clerk/nextjs";
-// import EmptyOrganization from "./_components/empty-org";
-
-// interface DashboardPageProps{
-//    searchParams: {
-//     search?: string;
-//     favourites?: string;
-//    }
-// }
-
-// const DashboardPage = ({}: DashboardPageProps) => {
-//   const {organization} = useOrganization();
-//   return (
-//     <div className='bg-dark-4 flex-1 pt-10 mt-3 rounded-lg h-[calc(100vh-80px)] overflow-auto p-6'>
-//        {JSON.stringify(searchParams)}
-//       <h1 className='text-white text-4xl font-bold mb-8'>Dashboard</h1>
-//       {!organization ? (
-//         <EmptyOrganization/>
-//       ) :(
-//         <p>Board List!</p>
-//       ) }
-     
-      
-   
-//       </div>
-
-//   );
-// }
-
-// export default DashboardPage;
 
 
 "use client";
