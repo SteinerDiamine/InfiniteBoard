259064ab13d298ec481a607e1894fe962e91ca01
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-09-23 11:09:27 +0530

    correction in dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,7 @@
-
-"use client";
-
+"use client"
 import { useOrganization } from "@clerk/nextjs";
 import EmptyOrganization from "./_components/empty-org";
-import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+
 
 interface DashboardPageProps {
   searchParams: {
@@ -11,28 +9,23 @@ interface DashboardPageProps {
     favourites?: string;
   }
 }
-
 const DashboardPage = ({ searchParams }: DashboardPageProps) => { // Destructure searchParams here
   const { organization } = useOrganization();
   
   return (
-    // <div className='bg-dark-4 flex-1 pt-10 mt-3 rounded-lg h-[calc(100vh-80px)] overflow-auto p-6'>
-    //   {JSON.stringify(searchParams)} {/* Now this will work correctly */}
-    //   <h1 className='text-white text-4xl font-bold mb-8'>Dashboard</h1>
-    //   {!organization ? (
-    //     <EmptyOrganization />
-    //   ) : (
-    //     <p>Board List!</p>
-    //   )}
-    // </div>
-    <BackgroundGradientAnimation>
-      <div className="flex-1 pt-10 mt-4 rounded-lg overflow-auto pb-7 ">
-        <h1 className="text-4xl">Dashboard</h1>
-      
-      </div>
-    </BackgroundGradientAnimation>
-  );
+    <div className='bg-dark-4 flex-1 pt-10 mt-3 rounded-lg h-[calc(100vh-80px)] overflow-auto p-6'>
+      {JSON.stringify(searchParams)} {/* Now this will work correctly */}
+      <h1 className='text-white text-4xl font-bold mb-8'>Dashboard</h1>
+
+
+
+{!organization ? (
+  <EmptyOrganization />
+) : (
+  <p>Board List!</p>
+)}
+</div>
+);
 }
 
 export default DashboardPage;
-
